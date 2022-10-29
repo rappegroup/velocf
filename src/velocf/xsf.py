1a827f176b80da12a0edde543bd3f8baccc71601
Aaron Schankler <ams19@sas.upenn.edu>
2022-10-29 12:52:36 -0400

    Add writer to xsf module

--- src/velocf/xsf.py
+++ src/velocf/xsf.py
@@ -1,6 +1,15 @@
 """Read for .xsf file format."""
 
-from typing import Iterable, Iterator, Optional, Tuple
+from typing import (
+    Callable,
+    Iterable,
+    Iterator,
+    List,
+    Optional,
+    Sequence,
+    Tuple,
+    TypeVar,
+)
 
 import numpy as np
 
@@ -123,3 +132,104 @@ def read_axsf(
         positions.append(step_pos)
 
     return Trajectory(basis, species, positions, "angstrom", var_cell=animate_cell)
+
+
+_T = TypeVar("_T")
+
+
+def columns(
+    matrix: Sequence[Sequence[_T]],
+    *,
+    min_space: int = 3,
+    left_pad: Optional[int] = None,
+    convert_fn: Callable[[_T], str] = str,
+    sep: str = " ",
+    align: str = "left",
+) -> List[str]:
+    """Format `matrix` into a string with aligned columns.
+    Args:
+        matrix: 2d array of values to print; may not be ragged
+        min_space: Minimum number of separators between columns
+        left_pad: Minimum number of leading separators (default `min_space`)
+        convert_fn: Conversion function applied to each element of `matrix` before
+            aligning (default :py:func:`str`)
+        sep: String used as a separator between columns (default space)
+        align: Direction to align elements if elements in a column are not the same
+            width. Either 'left' or 'right' (default 'left')
+    Returns:
+        Formatted matrix as a list of formatted lines
+    """
+    if align not in ("left", "right"):
+        raise ValueError(f"Incorrect alignment {align!r}")
+    if any(len(line) != len(matrix[0]) for line in matrix):
+        raise ValueError("Matrix may not be ragged")
+    if left_pad is None:
+        left_pad = min_space
+
+    field_strings = [list(map(convert_fn, line)) for line in matrix]
+    col_widths = [max(map(len, col)) for col in zip(*field_strings)]
+
+    complete_lines = []
+    for row in field_strings:
+        line = []
+        for width, field in zip(col_widths, row):
+            if align == "left":
+                line.append(field.ljust(width, sep))
+            else:
+                line.append(field.rjust(width, sep))
+        complete_lines.append(
+            left_pad * sep + (min_space * sep).join(line).rstrip() + "\n"
+        )
+    return complete_lines
+
+
+def _gen_primvec(basis: Basis, *, step: int = None) -> Iterator[str]:
+    if step is None:
+        yield "PRIMVEC\n"
+    else:
+        yield f"PRIMVEC {step:d}\n"
+    yield from columns(basis, convert_fn="{:.8f}".format)
+
+
+def _gen_primcoord(
+    species: Species, positions: Position, *, step: int = None
+) -> Iterator[str]:
+    if step is None:
+        yield "PRIMCOORD\n"
+    else:
+        yield f"PRIMCOORD {step:d}\n"
+    yield f"{len(species)} 1\n"
+    lines = [
+        (spec,) + tuple(map("{:.8f}".format, pos))
+        for spec, pos in zip(species, positions)
+    ]
+    yield from columns(lines, left_pad=0)
+
+
+def gen_xsf(struct: Structure) -> Iterator[str]:
+    """Generate lines for a single structure .xsf file."""
+    # pylint: disable=import-outside-toplevel
+    from velocf.cell import convert_positions
+
+    struct = convert_positions(struct, "angstrom")
+    yield "CRYSTAL\n"
+    yield from _gen_primvec(struct.basis)
+    yield from _gen_primcoord(struct.species, struct.positions)
+
+
+def gen_axsf(trajectory: Trajectory) -> Iterator[str]:
+    """Generate lines for an animated .axsf trajectory."""
+    # pylint: disable=import-outside-toplevel
+    from velocf.cell import convert_trajectory
+
+    trajectory = convert_trajectory(trajectory, "angstrom")
+    yield f"ANIMSTEPS {len(trajectory)}\n"
+    yield "CRYSTAL\n"
+
+    if not trajectory.var_cell:
+        yield from _gen_primvec(trajectory.basis)
+
+    for i, struct in enumerate(trajectory):
+        if trajectory.var_cell:
+            yield from _gen_primvec(struct.basis, step=i)
+        yield from _gen_primcoord(struct.species, struct.positions, step=i)
